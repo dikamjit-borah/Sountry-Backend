17e9db8f37a3c3ed127e6169316432e184b30ddc
biswajit-debnath <biswajitdebnath405@gmail.com>
2021-05-12 16:08:56 +0530

    database configured

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "soundtryuser",
+    "password": "password",
+    "database": "soundtrydb",
+    "host": "3.142.196.242",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
